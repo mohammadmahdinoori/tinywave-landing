3d33d6d2fc01e0ee9a8696db475ac8a74fbb952f
mohammadmahdinoori <mohammadmahdinoori70@gmail.com>
2025-06-30 00:58:00 +0300

    changed the footer title

--- src/app/landing/Footer.tsx
+++ src/app/landing/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
       <footer className="bg-black text-white text-center py-4">
-        <p className="text-sm">&copy; 2025 Flowence. All rights reserved.</p>
+        <p className="text-sm">&copy; 2025 Tinywave. All rights reserved.</p>
       </footer>
     );
   }
