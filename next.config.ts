fd9901816ebf13d5e909771735871c1b8131d3e6
mohammadmahdinoori <mohammadmahdinoori70@gmail.com>
2025-06-29 17:05:54 +0300

    Manual GitHub Pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // required for static export
+  },
+  basePath: '/tinywave-landing', // only if deploying to a subpath (i.e., GitHub Pages)
+  assetPrefix: '/tinywave-landing',
 };
 
 export default nextConfig;
