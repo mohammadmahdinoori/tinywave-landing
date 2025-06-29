af360a7522a2768b3d35773f0906a2c24321ae25
mohammadmahdinoori <mohammadmahdinoori70@gmail.com>
2025-06-30 01:10:44 +0300

    added the out file

--- src/app/landing/Footer.tsx
+++ src/app/landing/Footer.tsx
@@ -1,8 +1,7 @@
 export function Footer() {
-    return (
-      <footer className="bg-black text-white text-center py-4">
-        <p className="text-sm">&copy; 2025 Tinywave. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="bg-black text-white text-center py-4">
+      <p className="text-sm">&copy; 2025 Tinywave. All rights reserved.</p>
+    </footer>
+  );
+}
